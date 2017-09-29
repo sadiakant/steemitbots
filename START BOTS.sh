1544948503b84841d69eacea8965f561e7d9be8c
Siko91 <alexander.n.dinkov@gmail.com>
2017-09-30 00:59:03 +0300

    new start script for new OS

---
+++ START BOTS.sh
@@ -0,0 +1,4 @@
+
+cd /home/alex/Desktop/steemitbots/resteem-bot/; while true; do gnome-terminal -e "bash -c \"/home/alex/Documents/node-v6.11.3-linux-x64/bin/node /home/alex/Desktop/steemitbots/resteem-bot/resteem-bot.js\""; sleep 10; done
+
+cd /home/alex/Desktop/steemitbots/reblogger-bot/; while true; do gnome-terminal -e "bash -c \"/home/alex/Documents/node-v6.11.3-linux-x64/bin/node /home/alex/Desktop/steemitbots/reblogger-bot/reblogger-bot.js\""; sleep 10; done
